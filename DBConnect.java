943cccd412b0c28420b9c0f6eb816debda195cb3
Godfather_Kingko <kingkobkingko@yahoo.com>
2022-11-30 22:41:34 -0600

    updates on our first commit

--- DBConnect.java
+++ DBConnect.java
@@ -1,4 +1,5 @@
 This is a java file
 this file is created for practice purposes 
 This file will be commited to the staging area
+This file is our first commit for this project.
 Author of this work is Godfather_Kingko
